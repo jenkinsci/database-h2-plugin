128dca4e30c9d92ca23be358bcb16551251e4fe2
Kohsuke Kawaguchi <kk@kohsuke.org>
2012-11-25 12:24:58 -0800

    Moved the sample database console into the 'database' plugin

--- src/test/resources/org/jenkinsci/plugins/database/h2/Sample/config.groovy
+++
@@ -1,7 +0,0 @@
-package org.jenkinsci.plugins.database.h2.Sample;
-
-def f = namespace(lib.FormTagLib)
-
-f.section(title:"Database Configuration Example") {
-    f.dropdownDescriptorSelector(field:"database",title:_("Database"))
-}
