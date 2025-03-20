9afcae81a2066d8b6fdafb6247de59ebda24adf0
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-03-20 16:53:55 +0100

    Migrate tests to JUnit5 (#122)
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup

--- src/test/java/org/jenkinsci/plugins/database/h2/PerItemH2DatabaseTest.java
+++ src/test/java/org/jenkinsci/plugins/database/h2/PerItemH2DatabaseTest.java
@@ -1,55 +1,38 @@
 package org.jenkinsci.plugins.database.h2;
 
 import hudson.model.FreeStyleProject;
+import org.jenkinsci.plugins.database.PerItemDatabaseConfiguration;
+import org.junit.jupiter.api.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Arrays;
-import javax.sql.DataSource;
+
 import static org.hamcrest.CoreMatchers.is;
-import org.jenkinsci.plugins.database.PerItemDatabaseConfiguration;
-import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.Rule;
-import org.jvnet.hudson.test.JenkinsRule;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-public class PerItemH2DatabaseTest {
-    
-    @Rule public JenkinsRule j = new JenkinsRule();
+@WithJenkins
+class PerItemH2DatabaseTest {
 
-    @Test public void basics() throws Exception {
+    @Test
+    void basics(JenkinsRule j) throws Exception {
         FreeStyleProject p = j.jenkins.createProject(FreeStyleProject.class, "somejob");
         DataSource ds = PerItemDatabaseConfiguration.find().getDataSource(p);
-        Connection con = ds.getConnection();
-        try {
-            Statement st = con.createStatement();
-            try {
-                st.execute("create table FOO (a int, b int )");
-            } finally {
-                st.close();
+
+        try (Connection con = ds.getConnection();
+             Statement st = con.createStatement()) {
+
+            st.execute("create table FOO (a int, b int )");
+            st.execute("insert into FOO values (1,2)");
+
+            try (ResultSet r = st.executeQuery("select b from FOO where a=1")) {
+                r.next();
+                assertThat(r.getInt(1), is(2));
             }
-            st = con.createStatement();
-            try {
-                st.execute("insert into FOO values (1,2)");
-            } finally {
-                st.close();
-            }
-            st = con.createStatement();
-            try {
-                ResultSet r = st.executeQuery("select b from FOO where a=1");
-                try {
-                    r.next();
-                    assertThat(r.getInt(1), is(2));
-                } finally {
-                    r.close();
-                }
-            } finally {
-                st.close();
-            }
-        } finally {
-            con.close();
         }
-        System.err.println("XXX " + Arrays.asList(p.getRootDir().list()));
     }
 
 }
